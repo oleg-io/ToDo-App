dba486601cc1654b5b87f181f4b086f8d689c652
Oleg <filicim@gmail.com>
2022-03-21 23:29:43 +0500

    todo-app changes 21.03

--- todo-app.js
+++ todo-app.js
@@ -18,6 +18,7 @@ function createForm() {
   form.classList.add('todo-form');
   itemButton.classList.add('item-btn');
   itemButton.textContent = 'Добавить дело';
+  itemButton.disabled = true;
 
   buttonWrapper.append(itemButton);
   form.append(input);
@@ -41,10 +42,28 @@ function createTodoApp(container, title) {
   let todoHeader = createHeader(title);
   let todoForm = createForm();
   let todoList = createList();
-  
+//  let loadData = caseData();
+
   container.append(todoHeader);
   container.append(todoForm.form);
   
+  function activateButton() {
+    let button = document.querySelector('.item-btn');
+    let input = document.querySelector('input');
+
+    input.addEventListener('keyup', inpValue);
+
+    function inpValue() {
+      if (!input.value) {
+        return button.disabled = true;
+      } else {
+        return button.disabled = false;
+      };
+    };
+    return;
+  };
+  activateButton();
+
   todoForm.form.addEventListener('submit', function(e) {
   
       e.preventDefault();
@@ -58,6 +77,8 @@ function createTodoApp(container, title) {
   
       let todoItem = createItem(todoForm.input.value);
   
+
+
       todoItem.deleteButton.addEventListener('click', () => {
         todoItem.newItem.remove();
       });
@@ -69,6 +90,7 @@ function createTodoApp(container, title) {
       todoList.append(todoItem.newItem);
   
       todoForm.input.value = '';
+      todoForm.itemButton.disabled = true;
   
   });
   
@@ -78,7 +100,9 @@ function createTodoApp(container, title) {
       let groupButton = document.createElement('div');
       let doneButton = document.createElement('button');
       let deleteButton = document.createElement('button');
-      
+
+let data = 
+
       groupButton.classList.add('btn-container');
       newItem.classList.add('task');
       newItem.textContent = name;
@@ -100,6 +124,14 @@ function createTodoApp(container, title) {
   };
   
 };
+
+function caseData() {
+  let data = [];
+  console.log(data);
+  return data;
+};
+
+
 
 window.createTodoApp = createTodoApp;
 
